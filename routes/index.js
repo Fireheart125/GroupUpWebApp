d55ad5d2b769fd7da224bd31ceab8c8e9989758b
Hyungyun Kim <withyuns@gmail.com>
2016-11-07 17:37:10 -0800

    Finish setting

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,21 @@
 var mydata = require('../myGroups.json');
 
+
 function check(form) { /*function to check userid & password*/
-	var name = req.query.name;
-	var password = req.query.password;
-
-    if(name == mydata.name) {
-    	if(password == mydata.password) {
-    		console.log("inside password matching");
-    		window.location.href('main');
-    	}
-    	else {
-    		alert("Error Password or Username");/*displays error message*/
-    	}
-    }
-
+    console.log("Debug --- Enter the function check!!");
     /*the following code checkes whether the entered userid and password are matching*/
- /*   if(form.Name.value == name && form.Password.value == password) {
-    window.location.href('main')/*opens the target page while Id & password matches
+   if(form.Name.value == mydata.name && form.Password.value == mydata.password) {
+      window.location.href('main')/*opens the target page while Id & password matches */
+      console.log("Debug --- matching");
     }
     else {
-    	 alert("Error Password or Username")/*displays error message
-    	} */
+    	 alert("Error Password or Username")/*displays error message */
+         console.log("Debug --- NOT matching");
     }
+}
 
 exports.view = function(req, res){
-
+    console.log("Debug --- index.view");
 	res.render('index');
 };
+
--- routes/setting.js
+++ routes/setting.js
@@ -1,3 +1,36 @@
+var mydata = require('../myGroups.json');
+
 exports.view = function(req, res){
+
+   console.log ("DEBUG::: data? before");
+   console.log (mydata);
+
+   var name = req.query.name;
+   var email = req.query.email; 
+   var password = req.query.password;
+   console.log("IN THE SETTING");
+   console.log(name);
+   console.log(email);
+   console.log(password);
+
+   if(name != undefined && name != "") {
+      mydata.name.splice(0, 1);
+      mydata.name.push(name);
+   }
+
+   if(email != undefined && email != "") {
+      mydata.email.splice(0, 1);
+      mydata.email.push(email);
+   }
+
+   if(password != undefined && password != "") {
+      mydata.password.splice(0, 1);
+      mydata.password.push(password);
+   }
+
+
+   console.log ("DEBUG::: data? after");
+   console.log (mydata);
+
    res.render('setting');
 };
