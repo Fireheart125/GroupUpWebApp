cc0dfc0090ae29e04358d6ae879f54aa3a97f759
Wendy Lee <welee@ucsd.edu>
2016-11-07 16:02:22 -0800

    Still trying to firgure out log in

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,27 @@
+var mydata = require('../myGroups.json');
 
+function check(form) { /*function to check userid & password*/
+	var name = req.query.name;
+	var password = req.query.password;
+
+    if(name == mydata.name) {
+    	if(password == mydata.password) {
+    		console.log("inside password matching");
+    		window.location.href('main');
+    	}
+    	else {
+    		alert("Error Password or Username");/*displays error message*/
+    	}
+    }
+
+    /*the following code checkes whether the entered userid and password are matching*/
+ /*   if(form.Name.value == name && form.Password.value == password) {
+    window.location.href('main')/*opens the target page while Id & password matches
+    }
+    else {
+    	 alert("Error Password or Username")/*displays error message
+    	} */
+    }
 
 exports.view = function(req, res){
 
