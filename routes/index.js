3261566bcf5cb654442810f88f2b3212d371bd36
Doyoung Kim <dok083ucsd@gmail.com>
2016-11-09 19:09:51 -0800

    Log in fixed finally

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var classes = require('../data/allClasses.json');
 /* WTF */
 exports.signInAction = function(req, res){
    
-  console.log("singinIn Action");
+  console.log("sign in Action");
   console.log(res.body);
 
   console.log("Log in view");
@@ -39,6 +39,9 @@ exports.signInAction = function(req, res){
     }
   }
 
+  if(id == undefined && password == undefined)
+    return;
+
   /* If the user input is wrong */
   console.log("The user input is wrong!");
   res.render('LogInError');
@@ -60,70 +63,12 @@ exports.signInAction = function(req, res){
 /* No longer using this function */
 exports.view = function(req, res){
 
-  console.log ( req.body );
-
-
-  console.log("Log in view");
-  var id = req.query.id;
-  var password = req.query.password;
-
-  console.log("id and password");
-  console.log(id);
-  console.log(password);
-
-  console.log("my id and password");
-  for(var i=0; i < mydata.length; i++) {
-    console.log(mydata[i].id);
-    console.log(mydata[i].password);
-  }
-
-  console.log("Starting chekcing for loop");
-  for(i=0; i < mydata.length; i++) {
-    console.log(mydata[i].id);
-    console.log(mydata[i].password);
-
-    /* If the user is registered */
-    if(mydata[i].id == id) {
-      console.log("id check passed");
-      if(mydata[i].password == password) {
-        console.log("Check passed!");
-
-        var resultdata = mydata[i];
-
-        var resultdata2 = {
-            classes : classes,
-            mydata : resultdata,
-            groupNum : resultdata.group_own.length,
-        };
-        res.render('main', resultdata2);
-        return;
-      }
-    }
-  }
+  var id = req.body.id;
+  var password = req.body.password;
 
   if(id == undefined && password == undefined) {
     res.render('index');
   }
 
-  /* If the user input is wrong */
-  console.log("The user input is wrong!");
-  res.render('logInError');
-  return;
-  
-
-  /*
-  console.log(mydata.name[0] == name);
-  console.log(mydata.password[0] == password);
-  if(mydata.name[0] == name) {
-    console.log("Inside the if statement")
-    if(mydata.password[0] == password) {
-      console.log("Check passed!");
-      res.redirect('main');
-      return;
-    }
-  }
-  else {
-    res.render('index');
-    return;
-  }*/
+ 
 };
